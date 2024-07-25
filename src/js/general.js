4360990606ef11039d26e7c3992f761e2010d029
Ludovic Estival <ludovic.estival1@gmail.com>
2024-07-25 14:52:11 +0200

    Fixed nav error

--- src/js/general.js
+++ src/js/general.js
@@ -116,24 +116,6 @@ function addPostNav() {
     `;
 }
 
-// Balises Google Analytics
-document.getElementById("body").innerHTML +=
-`<!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NP3XX73Z"
-    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-`;
-
-document.getElementById("head").innerHTML +=
-`<!-- Google Tag Manager -->
-    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-    })(window,document,'script','dataLayer','GTM-NP3XX73Z');</script>
-    <!-- End Google Tag Manager -->
-`;
-
 // Librairies
 document.getElementById("head").innerHTML +=
 `<meta charset='utf-8'>
@@ -148,7 +130,7 @@ document.getElementById("head").innerHTML +=
 // Navigation
 var url = window.location.href
 
-if (url.includes('index.html')) {
+if (url.includes('index.html') || url == "https://ludovic-estival.github.io/") {
     addIndexNav();
 } else {
     split = url.split("/blog/")[1];
